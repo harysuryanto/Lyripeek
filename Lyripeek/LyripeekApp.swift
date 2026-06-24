0904cb84c12ff49d7810ac4b2c3efa62fcf0ac42
Hary Suryanto <hary.suryanto01@gmail.com>
2026-06-24 19:23:19 +0700

    Initial Commit

---
+++ Lyripeek/LyripeekApp.swift
@@ -0,0 +1,17 @@
+//
+//  LyripeekApp.swift
+//  Lyripeek
+//
+//  Created by Hary Suryanto on 24/06/26.
+//
+
+import SwiftUI
+
+@main
+struct LyripeekApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
